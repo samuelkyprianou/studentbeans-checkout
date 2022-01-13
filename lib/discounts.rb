4a32d4552caf752d43db3e75565d2b097b0c7c46
samuelkyprianou <samuelkyprianou@gmail.com>
2022-01-13 23:54:45 +0000

    percentage discounts subclass and offer creation added

--- lib/discounts.rb
+++ lib/discounts.rb
@@ -7,7 +7,7 @@ class Discounts
     @discounts_db = hash
   end
 
-    #write to discount DB with merging hash 
+  #write to discount DB with merging hash
   def create_offer(offers)
     discounts_db.merge!(offers)
   end
@@ -15,9 +15,20 @@ end
 
 class Multibuy_offers < Discounts
 
-    # Creation of multibuy offer takes the item, the amount needed to hit the offer and the number of free items
-    # e.g (:mango, 3, 1)
-    def add_multibuy_offer(item, buy, free)
-        offer = {item => [buy, free]}
-        self.create_offer(offer)
-    end
+  # Creation of multibuy offer takes the item, the amount needed to hit the offer and the number of free items
+  # e.g (:mango, 3, 1)
+  def add_multibuy_offer(item, buy, free)
+    offer = { item => [buy, free] }
+    self.create_offer(offer)
+  end
+end
+
+class Percentage_discounts < Discounts
+
+    # Creation of percentage discount takes the item and the value of percentage to apply
+    # 50% off bananas = (:bananas, 50)
+  def add_percentage_discount(item, value)
+    offer = { item => [value / 100.0] }
+    self.create_offer(offer)
+  end
+end
